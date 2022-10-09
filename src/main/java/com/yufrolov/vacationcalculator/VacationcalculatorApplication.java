267077a70a894ca63d5ca118a9b3b6cbac03c47c
yufrolov <yuriifrolov39@gmail.com>
2022-10-09 22:07:17 +0300

    initial commit

---
+++ src/main/java/com/yufrolov/vacationcalculator/VacationcalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.yufrolov.vacationcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacationcalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacationcalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yufrolov/vacationcalculator/VacationcalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yufrolov.vacationcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationcalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
